f1d436ce9647e14ad474a6a9ece402f820c91f77
opondo99 <markopondo.m@gmail.com>
2020-01-11 13:00:31 +0300

    phone view nav-bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Transition, animated } from 'react-spring/renderprops';
 import './navbar.css';
 
-const Nav = () => {
-    return (
-        <div className="nav">
-            <div className="logo">
-                <h1>Mark Opondo</h1>
+class Nav extends Component {
+    state = {
+        showNav: false
+    };
+
+    toggle = e => this.setState({
+        showNav: !this.state.showNav
+    });
+
+    render() {
+        return (
+            <div className="nav">
+                <div className="logo">
+                    <h1>Mark Opondo</h1>
+                </div>
+    
+                <div className="burger" onClick={this.toggle}>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </div>
+                <Transition
+                    native
+                    items={this.state.showNav}
+                    from={{ opacity:0, left:700}}
+                    enter={{ opacity:1, left: 210}}
+                    leave={{ opacity:0, left: 700}}
+                    config={{ delay: 300, duration:500}}
+                >
+                    {show => show && (props => (
+                        <animated.div style={props} className="phoneview">
+                            <ul className="links">
+                                <Link to="/">Home</Link>
+                                <Link to="/projects">Projects</Link>
+                                <Link to="/contact">Contact</Link>
+                            </ul>
+                        </animated.div>
+                    ))}
+                    
+                </Transition>
+                
             </div>
-            <ul className="links">
-                <Link to="/">Home</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </ul>
-        </div>
-    )
+        )
+    }
+    
 }
 
 export default Nav;
