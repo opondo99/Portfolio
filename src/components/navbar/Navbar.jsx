2a2a0c009d6ffab3d3f6676f4a5661fc7aa2c717
opondo99 <markopondo.m@gmail.com>
2020-01-11 15:13:54 +0300

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,36 +1,57 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Transition, animated } from 'react-spring/renderprops';
 import './navbar.css';
 
-class Nav extends Component {
-    state = {
-        showNav: false
+const Nav = () => {
+    const [isNavVisible, setIsNavVisible] = useState(false);
+    const [isSmallScreen, setIsSmallScreen ] = useState(false);
+
+
+    const toggle = e => {
+        setIsNavVisible(!isNavVisible);
     };
 
-    toggle = e => this.setState({
-        showNav: !this.state.showNav
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 600px)");
+        mediaQuery.addListener(handleMediaQueryChange);
+        handleMediaQueryChange(mediaQuery);
     });
 
-    render() {
-        return (
-            <div className="nav">
-                <div className="logo">
-                    <h1>Mark Opondo</h1>
-                </div>
-    
-                <div className="burger" onClick={this.toggle}>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                </div>
+    const handleMediaQueryChange = mediaQuery => {
+        if (mediaQuery.matches) {
+            setIsSmallScreen(true);
+        } else {
+            setIsSmallScreen(false);
+        }
+    };
+    // state = {
+    //     showNav: false
+    // };
+
+    // toggle = e => this.setState({
+    //     showNav: !this.state.showNav
+    // });
+
+    return (
+        <div className="nav">
+            <div className="logo">
+                <h1>Mark Opondo</h1>
+            </div>
+
+            <div className="burger" onClick={toggle}>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+            {( isNavVisible || !isSmallScreen ) && (
                 <Transition
                     native
-                    items={this.state.showNav}
+                    items={( isNavVisible || !isSmallScreen )}
                     from={{ opacity:0, left:700}}
                     enter={{ opacity:1, left: 210}}
                     leave={{ opacity:0, left: 700}}
-                    config={{ delay: 300, duration:500}}
+                    config={{ delay: 300, duration:400}}
                 >
                     {show => show && (props => (
                         <animated.div style={props} className="phoneview">
@@ -41,12 +62,14 @@ class Nav extends Component {
                             </ul>
                         </animated.div>
                     ))}
-                    
-                </Transition>
                 
-            </div>
-        )
-    }
+                </Transition>
+            )}
+                
+            
+        </div>
+    )
+    
     
 }
 
